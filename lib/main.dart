e6b85905e6ca88cc80f0455cb9456c67de7b9eb1
quzq <quzyam@gmail.com>
2021-07-27 19:52:31 +0900

    add ListViewBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,25 +29,40 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+String _jsonString = '''
+  [
+    {"title": "たいとる１", "score": 12 },
+    {"title": "たいｒｒとる１", "score": 32 },
+    {"title": "たいとる１", "score": 32 },
+    {"title": "たいｒとる１", "score": 3 },
+    {"title": "えたいとる１", "score": 2 },
+    {"title": "たいとるｒ１", "score": 3312 },
+    {"title": "たいえとる１", "score": 12 },
+    {"title": "たいえとるえ１", "score": 212 },
+  ]
+''';
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  List<String> _listData = [
-    'sassa',
-    'dsadasag',
-    'fdsfdfsase',
-    'sassa',
-    'dsadasag',
-    'fdsfdfsase',
-    'sassa',
-    'dsadasag',
-    'fdsfdfsase',
-    'sassa',
-    'dsadasag',
-    'fdsfdfsase',
-    'sassa',
-    'dsadasag',
-    'fdsfdfsaseassa',
-    'dsadasag',
+
+  //List<Map> _listData = jsonDecode(_jsonString);
+  List _listData = [
+    {'title': 'ssss', 'score': 23},
+    {'title': 'ssress', 'score': 32},
+    {'title': 'sstrss', 'score': 2},
+    {'title': 's443sss', 'score': 32},
+    {'title': 'ss7kss', 'score': 5232},
+    {'title': 'ssgdfsgsdss', 'score': 25},
+    {'title': 'ssfds4s', 'score': 522},
+    {'title': 'ss435ss', 'score': 25},
+    {'title': 'ssss', 'score': 23},
+    {'title': 'ssress', 'score': 32},
+    {'title': 'sstrss', 'score': 2},
+    {'title': 's443sss', 'score': 32},
+    {'title': 'ss7kss', 'score': 5232},
+    {'title': 'ssgdfsgsdss', 'score': 25},
+    {'title': 'ssfds4s', 'score': 522},
+    {'title': 'ss435ss', 'score': 25},
   ];
 
   Widget _menuItem(String title) {
@@ -72,17 +89,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: _listData.map((r) {
-            return _menuItem(r);
-          }).toList(),
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: Container(
+            width: double.infinity,
+            child: ListView.builder(
+                itemCount: _listData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text("* " + _listData[index].toString()),
+                  );
+                })));
   }
 }
